9e1b5d7dc785d875accbf1341b6c1589f93aa401
paolos04-prog <paolo.stumbo13@gmail.com>
2026-04-23 13:15:45 +0200

    last update (23-04)

--- 17-04.py
+++ 17-04.py
@@ -11,6 +11,9 @@ C = 0.0025 #capacitance [F]
 L = 1   #inductance [H]
 
 dE = lambda t: 0.2*np.sin(2.5*t)    #function of the derivative of the electrical field
+to_run = 'solution'     # string to chance to run different parts of the code: 
+# 'solution' compares different methods for solving the ivp problem
+# 'consistency' implements the consistency study
 
 ## ---------------------------------------------------------------------- ##
 ## ONE-STEP RESOLUTION METHODS
@@ -69,24 +72,6 @@ def CN(x0, nodes):
     
     return x, ccn
 
-y_ee, C_ee, time = EE(y0,250)
-y_rk2, C_rks = RK2(y0,250)
-y_cn, C_cn = CN(y0,250)
-dt = time[1]-time[0]
-
-## PLOTS OF i WITH THE DIFFERENT METHODS ##
-plt.figure(1, figsize=(12,5))
-plt.plot(time, y_ee[0,:], 'b-', linewidth = 3, label = "EE")
-plt.plot(time, y_rk2[0,:], 'r--', linewidth = 2, label = "RK2")
-plt.plot(time, y_cn[0,:], 'g-', label = "CN")
-plt.legend(loc = 'best')
-plt.xlabel('t(s)')
-plt.ylabel('i(A)')
-plt.title('current with one-step methods for time step:' + str(dt) +'s')
-plt.grid(True)
-plt.show()
-
-
 ## ---------------------------------------------------------------------- ##
 ## NEWMARK RESOLUTION METHODS
 
@@ -113,71 +98,82 @@ def verlet(x0, nodes):
 
     return y, yprime
 
-i_ver, ip_ver = verlet(y0, 250)
+match to_run:
 
-## PLOTS
-plt.figure(2, figsize=(12,5))
-plt.plot(time, i_ver, 'y-', label = "Verlet")
-plt.legend(loc = 'best')
-plt.xlabel('t(s)')
-plt.ylabel('i(A)')
-plt.title('current with verlet method for time step:' + str(dt) +'s')
-plt.grid(True)
-plt.show()
+    case 'solution':
+        y_ee, C_ee, time = EE(y0,250)
+        y_rk2, C_rks = RK2(y0,250)
+        y_cn, C_cn = CN(y0,250)
+        i_ver, ip_ver = verlet(y0, 250)
+        dt = time[1]-time[0]
 
-## ---------------------------------------------------------------------- ##
-## CONSISTENCY STUDY
+        ## PLOTS OF i WITH THE DIFFERENT METHODS ##
+        plt.figure(figsize=(12,5))
+        plt.plot(time, y_ee[0,:], 'b-', linewidth = 3, label = "EE")
+        plt.plot(time, y_rk2[0,:], 'r--', linewidth = 2, label = "RK2")
+        plt.plot(time, y_cn[0,:], 'g-.', linewidth = 2, label = "CN")
+        plt.plot(time, i_ver, 'y-', label = "Verlet")
+        plt.legend(loc = 'best')
+        plt.xlabel('t(s)')
+        plt.ylabel('i(A)')
+        plt.title('current with different methods for time step:' + str(dt) +'s')
+        plt.grid(True)
+        plt.show()
 
-# In this part of the code some a consistency study is developed
-# To do the stability study, we evaluate the relative error
-# In order to do that we consider the reference solution as the one obtained with the CN method (the most stable and precise) with a very small deltat (say with 1000 nodes ==- t=0.005s)
-# We will compare the current at 5s (i.e. the last value of the vector containing the currents)
-# Than we compare, for different time intervals, the norm of the relative errors. the time intervals will be taken from 0.005 to 0.02 s (in order to consider the EE stability radius)
-# Lastly we plot (on a log-log graph) the evolution of the error in function of the time interval. The slope of the line should be an approximation of the order of convergence (found with polyfit)
+    case 'consistency':
+        ## ---------------------------------------------------------------------- ##
+        ## CONSISTENCY STUDY
 
-## ---------------------------------------------------------------------- ##
+        # In this part of the code some a consistency study is developed
+        # To do the stability study, we evaluate the relative error
+        # In order to do that we consider the reference solution as the one obtained with the CN method (the most stable and precise) with a very small deltat (say with 1000 nodes ==- t=0.005s)
+        # We will compare the current at 5s (i.e. the last value of the vector containing the currents)
+        # Than we compare, for different time intervals, the norm of the relative errors. the time intervals will be taken from 0.005 to 0.02 s (in order to consider the EE stability radius)
+        # Lastly we plot (on a log-log graph) the evolution of the error in function of the time interval. The slope of the line should be an approximation of the order of convergence (found with polyfit)
 
-# getting the reference solution
-sol = CN(y0, 5000)
-ref_sol = sol[0][0,-1]
+        ## ---------------------------------------------------------------------- ##
 
-nodes = np.linspace(2000,500,100)
-deltat = np.zeros(len(nodes))
-relerr_ee = np.zeros(len(deltat))
-relerr_rk2 = np.zeros(len(deltat))
-relerr_cn = np.zeros(len(deltat))
-relerr_ver = np.zeros(len(deltat))
+        # getting the reference solution
+        sol = CN(y0, 5000)
+        ref_sol = sol[0][0,-1]
 
-for i in range(0, len(deltat)):
-    deltat[i] = 5/int(nodes[i])
+        nodes = np.linspace(2000,500,100)
+        deltat = np.zeros(len(nodes))
+        relerr_ee = np.zeros(len(deltat))
+        relerr_rk2 = np.zeros(len(deltat))
+        relerr_cn = np.zeros(len(deltat))
+        relerr_ver = np.zeros(len(deltat))
 
-    sol_EE = EE(y0, int(nodes[i]))
-    relerr_ee[i] = np.abs((sol_EE[0][0,-1]-ref_sol)/ref_sol)
-    sol_rk2 = RK2(y0, int(nodes[i]))
-    relerr_rk2[i] = np.abs((sol_rk2[0][0,-1]-ref_sol)/ref_sol)
-    sol_CN = CN(y0, int(nodes[i]))
-    relerr_cn[i] = np.abs((sol_CN[0][0,-1]-ref_sol)/ref_sol)
-    sol_ver = verlet(y0, int(nodes[i]))
-    relerr_ver[i] = np.abs((sol_ver[0][-1]-ref_sol)/ref_sol)
+        for i in range(0, len(deltat)):
+            deltat[i] = 5/int(nodes[i])
 
-# ORDER OF CONVERGENCE ESTIMATION
+            sol_EE = EE(y0, int(nodes[i]))
+            relerr_ee[i] = np.abs((sol_EE[0][0,-1]-ref_sol)/ref_sol)
+            sol_rk2 = RK2(y0, int(nodes[i]))
+            relerr_rk2[i] = np.abs((sol_rk2[0][0,-1]-ref_sol)/ref_sol)
+            sol_CN = CN(y0, int(nodes[i]))
+            relerr_cn[i] = np.abs((sol_CN[0][0,-1]-ref_sol)/ref_sol)
+            sol_ver = verlet(y0, int(nodes[i]))
+            relerr_ver[i] = np.abs((sol_ver[0][-1]-ref_sol)/ref_sol)
 
-p_ee = np.polyfit(np.log(deltat),np.log(relerr_ee),1)
-p_rk2 = np.polyfit(np.log(deltat),np.log(relerr_rk2),1)
-p_cn = np.polyfit(np.log(deltat),np.log(relerr_cn),1)
-p_ver = np.polyfit(np.log(deltat),np.log(relerr_ver),1)
+        # ORDER OF CONVERGENCE ESTIMATION
 
-#plots
+        p_ee = np.polyfit(np.log(deltat),np.log(relerr_ee),1)
+        p_rk2 = np.polyfit(np.log(deltat),np.log(relerr_rk2),1)
+        p_cn = np.polyfit(np.log(deltat),np.log(relerr_cn),1)
+        p_ver = np.polyfit(np.log(deltat),np.log(relerr_ver),1)
 
-plt.figure(3, figsize=(12,5))
-plt.loglog(deltat, relerr_ee, 'b-', label = 'EE, p = '+str(p_ee[0]))
-plt.loglog(deltat, relerr_rk2, 'r-', label = 'RK2, p = '+str(p_rk2[0]))
-plt.loglog(deltat, relerr_cn, 'g-', label = 'CN, p = '+str(p_cn[0]))
-plt.loglog(deltat, relerr_ver, 'y-', label = 'VERLET, p = '+str(p_ver[0]))
+        #plots
 
-plt.legend(loc ='best')
-plt.xlabel('Delta t')
-plt.ylabel('Relative error')
-plt.title('Consistency study')
-plt.grid(True, which = 'both')
-plt.show()
+        plt.figure(figsize=(12,5))
+        plt.loglog(deltat, relerr_ee, 'b-', label = 'EE, p = '+str(p_ee[0]))
+        plt.loglog(deltat, relerr_rk2, 'r-', label = 'RK2, p = '+str(p_rk2[0]))
+        plt.loglog(deltat, relerr_cn, 'g-', label = 'CN, p = '+str(p_cn[0]))
+        plt.loglog(deltat, relerr_ver, 'y-', label = 'VERLET, p = '+str(p_ver[0]))
+
+        plt.legend(loc ='best')
+        plt.xlabel('Delta t')
+        plt.ylabel('Relative error')
+        plt.title('Consistency study')
+        plt.grid(True, which = 'both')
+        plt.show()
